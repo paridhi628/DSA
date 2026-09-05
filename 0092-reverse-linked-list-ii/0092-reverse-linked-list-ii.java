a469327458807d2dd6296743ef55ffb3e0612c62
paridhi628 <paridhiparv2706@gmail.com>
2026-09-05 13:07:00 +0530

    Time: 0 ms (100%), Space: 43.2 MB (12.22%) - LeetHub

--- 0092-reverse-linked-list-ii/0092-reverse-linked-list-ii.java
+++ 0092-reverse-linked-list-ii/0092-reverse-linked-list-ii.java
@@ -9,32 +9,64 @@
  * }
  */
 class Solution {
-    public ListNode reverseBetween(ListNode head, int left, int right) {
-        if(head == null || left == right)
-            return head;
-        ListNode temp1=head;
-       ListNode prevLeft=null;
-       for(int i=1;i<left;i++){
-        prevLeft=temp1;
-        temp1=temp1.next;
-       }
-       ListNode curr=temp1;
+    public static void reverse(ListNode head){
+         ListNode curr=head;
        ListNode prev=null;
        ListNode fwd=null;
-       for(int i=left;i<=right;i++){
+       while(curr!=null){
         fwd=curr.next;
         curr.next=prev;
         prev=curr;
         curr=fwd;
-       }
-       if(prevLeft!=null){
-        prevLeft.next=prev;
-       }
-       else {
-        head=prev;
-       }
-       temp1.next=curr;
-       return head;
-
     }
+    }
+    public ListNode reverseBetween(ListNode head, int left, int right) {
+    //     if(head == null || left == right)
+    //         return head;
+    //     ListNode temp1=head;
+    //    ListNode prevLeft=null;
+    //    for(int i=1;i<left;i++){
+    //     prevLeft=temp1;
+    //     temp1=temp1.next;
+    //    }
+    //    ListNode curr=temp1;
+    //    ListNode prev=null;
+    //    ListNode fwd=null;
+    //    for(int i=left;i<=right;i++){
+    //     fwd=curr.next;
+    //     curr.next=prev;
+    //     prev=curr;
+    //     curr=fwd;
+    //    }
+    //    if(prevLeft!=null){
+    //     prevLeft.next=prev;
+    //    }
+    //    else {
+    //     head=prev;
+    //    }
+    //    temp1.next=curr;
+    //    return head;
+    if (head == null || left == right) {
+            return head;
+        }
+    ListNode dummy=new ListNode(-1);
+    ListNode temp=dummy;
+    dummy.next=head;
+    for(int i=1;i<=left-1;i++){
+        temp=temp.next;
+    }
+    ListNode tail1=temp;
+    ListNode head2=temp.next;
+    for(int i=1;i<=right-left+1;i++){
+        temp=temp.next;
+    }
+    ListNode tail2=temp;
+    ListNode head3=temp.next;
+    tail1.next=null;
+    tail2.next=null;
+     reverse(head2);
+    tail1.next = tail2;
+    head2.next=head3;
+    return dummy.next;
+}
 }
